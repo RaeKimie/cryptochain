0a22ae6f51bec3596fb8b46a42b8eb531dc4bb56
Narae Kim <54043199+RaeKimie@users.noreply.github.com>
2021-05-26 00:02:02 +0100

    add test for genesis() function

--- block.test.js
+++ block.test.js
@@ -1,4 +1,5 @@
 const Block = require('./block');
+const { GENESIS_DATA } = require('./config');
 
 describe('Block', () => {
 	it('should have a timestamp, lastHash, hash and data property', () => {
@@ -15,5 +16,14 @@ describe('Block', () => {
 		expect(block.data).toEqual(data.data);
 	});
 
+	describe('genesis()', () => {
+		const genesisBlock = Block.genesis();
 
+		it('should return a Block instance', () => {
+			expect(genesisBlock).toBeInstanceOf(Block);
+		})
+		it('should return the genesis data', () => {
+			expect(genesisBlock).toEqual(GENESIS_DATA);
+		})
+	})
 })
