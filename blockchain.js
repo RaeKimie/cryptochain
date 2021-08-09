aeee22d72de6a508bf7af9be8576dbef75b9d3ec
Narae Kim <54043199+RaeKimie@users.noreply.github.com>
2021-08-09 01:04:25 +0100

    add isValidChain

--- blockchain.js
+++ blockchain.js
@@ -1,15 +1,41 @@
 const Block = require('./block');
+const cryptoHash = require('./crypto-hash');
 class Blockchain {
 	constructor() {
 		this.chain = [Block.genesis()];
 	}
 
+	//add new block to chain
 	addBlock({ data }) {
-		const lastBlock = this.chain.length-1;
+		const lastBlock = this.chain[this.chain.length-1];
 		const newBlock = Block.mineBlock({ lastBlock, data});
 		this.chain.push(newBlock);
 	}
 
+	static isValidChain(chain) {
+		if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+			return false;
+		}
+		for (let i = 1; i < chain.length; i++){
+			const {timestamp, lastHash, hash, data} = chain[i];
+			const actualLastHash = chain[i-1].hash;
+
+			if ( lastHash !== actualLastHash) {
+				return false;
+			}
+			if ( Object.keys(chain[i]).length > 4) {
+				return false;
+			}
+
+			const validHash = cryptoHash(timestamp, lastHash, data);
+			if (hash !== validHash) {
+
+			return false;
+			}
+		}
+
+		return true;
+	}
 }
 
 module.exports = Blockchain;
